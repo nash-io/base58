f07300c1289124839da5c2c1fc10192a69d30bb8
Adrien <adrien.moreau@backslash.fr>
2019-05-13 15:38:43 +0200

    improve perf

--- test/base58_test.exs
+++ test/base58_test.exs
@@ -2,16 +2,16 @@ defmodule Base58Test do
   use ExUnit.Case
 
   test "encode" do
-    assert Base58.encode(0) == "1"
-    assert Base58.encode(57) == "Z"
-    assert Base58.encode(1024) == "iE"
-    assert Base58.encode(123456789) == "bUKpk"
+    assert Base58.encode(<<0>>) == "1"
+    assert Base58.encode(<<57>>) == "z"
+    assert Base58.encode(:binary.encode_unsigned(1024)) == "Jf"
+    assert Base58.encode(:binary.encode_unsigned(123_456_789)) == "BukQL"
   end
 
   test "decode" do
-    assert Base58.decode("1") == 0
-    assert Base58.decode("Z") == 57
-    assert Base58.decode("iE") == 1024
-    assert Base58.decode("bUKpk") == 123456789
+    assert Base58.decode("1") == <<0>>
+    assert Base58.decode("z") == <<57>>
+    assert Base58.decode("Jf") == :binary.encode_unsigned(1024)
+    assert Base58.decode("BukQL") == :binary.encode_unsigned(123_456_789)
   end
 end
